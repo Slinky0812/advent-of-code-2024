7131c3240070d6235535d17f1669c0ecfcbed6ae
Slinky0812 <abdullahalimoledina@gmail.com>
2024-12-01 13:48:54 +0000

    Day 1 Part 1 Complete

---
+++ Day 1/day-1-part-1.py
@@ -0,0 +1,48 @@
+def generateLists(path):
+    word = ''
+    leftWords = []
+    rightWords = []
+    with open(path) as file:
+        left = True
+        while True:
+            char = file.read(1)
+            if char.isspace():
+                if word:
+                    if left:
+                        leftWords.append(word)
+                        left = False
+                    else:
+                        rightWords.append(word)
+                        left = True
+                    word = ''
+            elif char == '':
+                if word:
+                    if left:
+                        leftWords.append(word)
+                        left = False
+                    else:
+                        rightWords.append(word)
+                        left = True
+                break
+            else:
+                word += char
+
+    return leftWords, rightWords
+
+# Find the Left list and right list
+left, right = generateLists('location-id-list.txt')
+
+# Sort out lists from smallest to largest
+left.sort()
+right.sort()
+
+distance = 0
+# len(left) == len(right)
+for id in range(0, len(left)):
+    if int(right[id]) > int(left[id]):
+        currentDistance = int(right[id]) - int(left[id])
+    else:
+        currentDistance = int(left[id]) - int(right[id])
+    distance = distance + currentDistance
+
+print(distance)
